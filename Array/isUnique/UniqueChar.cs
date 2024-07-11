8cca72e3ed78c7aa8e18585bbb1d2c7ffa601866
DAT NGUYEN <dnguyenr@uw.edu>
2024-07-10 17:28:47 -0700

    push first problem

---
+++ Array/isUnique/UniqueChar.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 
+{
+    public class UniqueChar
+    {
+        public bool string Unique (string str){
+            if (str.Length > 128){
+                return false;
+            }
+            bool[] char = new bool[128];
+            for each (char c in str){
+                //convert char to ASCII
+                int val = (int) c;
+                //check if char has been seen before
+                if (char[val]){
+                    return false;
+                }
+                char[val] = true;
+            }
+            return true;
+        }
+    }
+}
